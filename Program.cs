073b667aab6d31850bf0ac0f7d1bd05e2c5cda7f
Astudillo <huitzilopochtli@astudillo.com>
2020-12-06 19:44:49 -0600

    First commit on day 6, day 4 ex 2 pending

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using AdventCode.Exercises;
+using System;
+
+namespace AdventCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // Day 1.
+            // Day1.Exercise1();
+            // Day1.Exercise2();
+
+            // Day 2.
+            // Day2.Exercise1();
+            // Day2.Exercise2();
+
+            // Day 3.
+            // var day3Ex1 = Day3.Exercise1(3,1);
+            // var day3ex2 = Day3.Exercise2();
+
+            // Day 4.
+            // var day4Ex1 = Day4.Exercise1();
+            // var day4Ex2 = Day4.Exercise2();
+
+            // Day 5.
+            // var day5Ex1 = Day5.Exercise1();
+            // var day5Ex2 = Day5.Exercise2();
+
+            // Day6.
+            // var day6Ex1 = Day6.Exercise1();
+            var day6Ex2 = Day6.Exercise2();
+        }
+    }
+}
